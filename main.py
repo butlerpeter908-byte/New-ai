d48b340a4c05db64624c3aaf22a03469b881ac0b
Butler peter <254298063+butlerpeter908-byte@users.noreply.github.com>
2026-07-11 21:42:26 +0530

    Change application name from 'SIDDIQUE AI' to 'NEXUS AI'

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def send_mail(to, sub, body):
 
 # ================= 4. LOGIN =================
 if not st.session_state.logged_in:
-    st.markdown("<div class='chat-card' style='text-align:center'><h1>SIDDIQUE AI</h1><p>System Authentication Required</p></div>", unsafe_allow_html=True)
+    st.markdown("<div class='chat-card' style='text-align:center'><h1>NEXUS AI</h1><p>System Authentication Required</p></div>", unsafe_allow_html=True)
     email = st.text_input("Enter Email ID")
     if not st.session_state.otp_sent:
         if st.button("Initialize Access"):
